515885a807880dfc8ef9ad8fc1c8d3189ff9b581
HDzungx <hdzungx@gmail.com>
2024-11-03 12:41:08 +0700

    miuicamera: Use prebuilt vendor.qti.hardware.camera.device@1.0 differently Fixes: error: module prebuilt_vendor.qti.hardware.camera.device@1.0 variant android_vendor_arm64_armv8-2a-dotprod_cortex-a76_shared: partition is different: system_ext(vendor.qti.hardware.camera.devi ce@1.0) != vendor(prebuilt_vendor.qti.hardware.camera.device@1.0)

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --remove-needed libhidltransport.so "${2}"
             ;;
+        vendor/bin/hw/android.hardware.camera.provider@2.4-service_64 | vendor/lib/android.hardware.camera.provider@2.4-legacy.so | vendor/lib/camera.device@1.0-impl.so | vendor/lib64/android.hardware.camera.provider@2.4-legacy.so | vendor/lib64/camera.device@1.0-impl.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "vendor.qti.hardware.camera.device@1.0.so" "vendor.qti.hardware.camera.device@1.0_vendor.so" "${2}"
+            ;;
         *)
             return 1
             ;;
--- miuicamera-vendor.mk
+++ miuicamera-vendor.mk
@@ -25,7 +25,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.5-impl \
     camera.device@3.6-external-impl \
     android.hardware.camera.provider@2.4-impl \
-    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
     vendor.qti.hardware.camera.device@2.0 \
     vendor.qti.hardware.camera.device@3.5 \
     MiuiCamera \
